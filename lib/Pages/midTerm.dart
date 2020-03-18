ae7a57dd6072c7331a61f35626fec0f0b2e2c271
Michi265 <michela.crulli5@gmail.com>
2020-03-18 18:09:39 +0100

    draw

--- lib/Pages/midTerm.dart
+++ lib/Pages/midTerm.dart
@@ -1,58 +1,31 @@
+import 'dart:math';
+import 'dart:ui';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:agemob/Pages/midTermPointer.dart';
 import 'package:flutter/material.dart';
 
 class MidTerm extends StatefulWidget {
-  final double width;
-  final double height;
-  final Color color;
 
-  const MidTerm({
-    this.width = 300.0,
-    this.height = 50.0,
-    this.color = Colors.black,
-  });
   @override
   _MidTermState createState() => _MidTermState();
 }
 
 class _MidTermState extends State<MidTerm> {
 
-  double _dragPosition = 0;
-  double _dragPercentage = 0;
-
-  void _updateDragPosition(Offset val){
-   double newDragPosition = 0;
-
-   if(val.dx <= 0){
-     newDragPosition = 0;
-   }else if (val.dx >= widget.width){
-     newDragPosition = widget.width;
-   }else{
-     newDragPosition = val.dx;
-   }
-
-   setState(() {
-     _dragPosition = newDragPosition;
-     _dragPercentage = _dragPosition/ widget.width;
-   });
-
-  }
-
-  void _onDragUpdate(BuildContext context, DragUpdateDetails update){
-    RenderBox box = context.findRenderObject();
-    Offset offset = box.globalToLocal(update.globalPosition);
-    _updateDragPosition(offset);
-  }
-  void _onDragStart(BuildContext context, DragStartDetails start){
-    RenderBox box = context.findRenderObject();
-    Offset localOffset = box.globalToLocal(start.globalPosition);
-    _updateDragPosition(localOffset);
-  }
-  void _onDragEnd(BuildContext context, DragEndDetails end){
-    setState(() {
-    });
-  }
-
+  Color selectedColor = Colors.black;
+  Color pickerColor = Colors.black;
+  double strokeWidth = 3.0;
+  List<DrawingPoints> points = List();
+  bool showBottomList = false;
+  double opacity = 1.0;
+  SelectedMode selectedMode = SelectedMode.StrokeWidth;
+  List<Color> colors = [
+    Colors.red,
+    Colors.green,
+    Colors.blue,
+    Colors.amber,
+    Colors.black
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -84,35 +57,140 @@ class _MidTermState extends State<MidTerm> {
           ],
         ),
       ),
-      body:  Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Container(
-            child: GestureDetector(
-              child: Container(
-                width: widget.width,
-                height: widget.height,
-                //qui c'era il colore rosso quindi probabilmente quelli
-                //sopra sono i parametri per creare il rettangolo e te farai un triangolo
-                child: CustomPaint(
-                  painter: MidTermPointer(
-                    color: widget.color,
-                    dragPercentage: _dragPercentage,
-                    sliderPosition: _dragPosition,
-                  ),
-                ),
-              ),
-              onHorizontalDragUpdate: (DragUpdateDetails update) =>
-                  _onDragUpdate(context, update),
-              onHorizontalDragStart: (DragStartDetails start) =>
-                  _onDragStart(context, start),
-              onHorizontalDragEnd: (DragEndDetails end) =>
-                  _onDragEnd(context, end),
-            ),
+      body: GestureDetector(
+        onPanUpdate: (details) {
+          setState(() {
+            RenderBox renderBox = context.findRenderObject();
+            points.add(DrawingPoints(
+                points: details.globalPosition,
+                //renderBox.globalToLocal(details.globalPosition),
+                paint: Paint()
+                  ..isAntiAlias = true
+                  ..color = selectedColor.withOpacity(opacity)
+                  ..strokeWidth = strokeWidth));
+          });
+        },
+        onPanStart: (details) {
+          setState(() {
+            RenderBox renderBox = context.findRenderObject();
+            points.add(DrawingPoints(
+                points: details.globalPosition,
+                paint: Paint()
+                  ..isAntiAlias = true
+                  ..color = selectedColor.withOpacity(opacity)
+                  ..strokeWidth = strokeWidth));
+          });
+        },
+        onPanEnd: (details) {
+          setState(() {
+            points.add(null);
+          });
+        },
+        child: CustomPaint(
+          size: Size.infinite,
+          painter: DrawingPainter(
+            pointsList: points,
           ),
         ),
-      );
-
-
+      ),
+    );
   }
 }
+
+
+class DrawingPainter extends CustomPainter {
+  DrawingPainter({
+    this.pointsList,
+
+  });
+  List<DrawingPoints> pointsList;
+  List<Offset> offsetPoints = List();
+
+  bool init = false;
+  var workLeftX = new List(10);
+  var workLeftY= new List(10);
+  var workRightX = new List(10);
+  var workRightY = new List(10);
+
+  void paintCerchio(Canvas canvas, Size size){
+    double stepWorkY = (((size.width/2)-15)*sin(pi/3))/10;
+    double stepWorkX = (((size.width/2)-15)*cos(pi/3))/10;
+
+    for(int i= 0; i < 10; i++) {
+      this.workRightX[i] = (size.width / 2) - (i * stepWorkX);
+      this.workLeftX[i] = (size.width / 2) + (i * stepWorkX);
+
+      this.workRightY[i] = (size.height / 2) - (i * stepWorkY);
+      this.workLeftY[i] = (size.height / 2) - (i * stepWorkY);
+    }
+      canvas.drawLine(Offset(size.width / 2, size.height / 2),
+          Offset(this.workRightX[this.workRightX.length-1], this.workRightY[this.workRightY.length-1]), Paint()
+            ..color = Colors.black
+            ..strokeWidth = 10);
+
+      canvas.drawLine(Offset(size.width / 2, size.height / 2),
+          Offset(this.workLeftX[this.workLeftX.length-1], this.workLeftY[this.workLeftY.length-1]), Paint()
+            ..color = Colors.black
+            ..strokeWidth = 10);
+  }
+  double distance( double x1, double y1, double x2, double y2){
+    return sqrt(pow((x1-x2), 2)+ pow((y1-y2), 2));
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    /*
+    for (int i = 0; i < pointsList.length - 1; i++) {
+      if (pointsList[i] != null && pointsList[i + 1] != null) {
+        //canvas.drawLine(pointsList[i].points, pointsList[i + 1].points,
+            //pointsList[i].paint);
+      } else if (pointsList[i] != null && pointsList[i + 1] == null) {
+        offsetPoints.clear();
+        offsetPoints.add(pointsList[i].points);
+        offsetPoints.add(Offset(
+            pointsList[i].points.dx + 0.1, pointsList[i].points.dy + 0.1));
+        //canvas.drawPoints(PointMode.points, offsetPoints, pointsList[i].paint);
+      }
+    }
+
+     */
+    if(this.init == false){
+      paintCerchio(canvas, size);
+      this.init = true;
+    }
+    if (pointsList != null && pointsList.length >= 2) {
+
+      double best_distance = 10000;
+      var best_index;
+
+      for(int i= 0; i < 10; i++) {
+        double d = distance(pointsList[pointsList.length - 1].points.dx,pointsList[pointsList.length - 1].points.dy, this.workRightX[i],this.workRightY[i])
+            + distance(pointsList[pointsList.length - 1].points.dx,pointsList[pointsList.length - 1].points.dy, this.workLeftX[i],this.workLeftY[i]);
+        if (d < best_distance){
+          best_distance = d;
+          best_index = i;
+        }
+      }
+
+      Path path = new Path();
+      path.moveTo(size.width / 2, size.height / 2);
+      path.lineTo(this.workRightX[best_index],
+          this.workRightY[best_index]);
+      path.lineTo(this.workLeftX[best_index],
+          this.workLeftY[best_index]);
+      canvas.drawPath(path, Paint()
+        ..color = Colors.pinkAccent);
+    }
+  }
+
+  @override
+  bool shouldRepaint(DrawingPainter oldDelegate) => true;
+}
+
+class DrawingPoints {
+  Paint paint;
+  Offset points;
+  DrawingPoints({this.points, this.paint});
+}
+
+enum SelectedMode { StrokeWidth, Opacity, Color }
--- lib/Pages/midTermPointer.dart
+++ lib/Pages/midTermPointer.dart
@@ -30,8 +30,8 @@ class MidTermPointer extends CustomPainter{
     _paintAnchors(canvas, size);
     _paintLine(canvas, size);
     _paintBlock(canvas, size);
-  }
 
+  }
 
   _paintAnchors(Canvas canvas, Size size){
     //canvas.drawCircle(Offset(0.0, size.height), 5.0, fillPainter);
@@ -39,24 +39,58 @@ class MidTermPointer extends CustomPainter{
   }
 
   _paintBlock(Canvas canvas, Size size){
-    Rect sliderRect = Offset(sliderPosition, size.height - 150.0) & Size(3.0, 200.0);
-    canvas.drawRect(sliderRect, fillPainter);
+    //Rect sliderRect = Offset(sliderPosition, size.height - 150.0) & Size(3.0, 200.0);
+    //rotate(canvas, size.width / 2, size.height - 50, 22.5);
+    //canvas.drawRect(sliderRect, fillPainter);
   }
   _paintLine(Canvas canvas, Size size) {
+    /*
     Path path = Path();
-    path.moveTo(size.width/2, 0);
+    path.moveTo(size.width / 2, 0);
     //path.arcTo(rect, sliderPosition/2, sliderPosition, false);
     path.lineTo(sliderPosition, size.height - 150.0);
     path.lineTo(sliderPosition, size.height + 50.0);
     //path.close();
+    rotate(canvas, size.width / 2, size.height - 50, -22.5);
     canvas.drawPath(path, fillPainter);
-    canvas.drawCircle(Offset(size.width/2,size.height- 50), 150, termPainter);
+    rotate(canvas, size.width / 2, size.height - 50, 22.5);
     Path path1 = Path();
     //Rect sliderRect1 = (Offset(size.width/2,size.height- 200))& Size(2.0, 300.0);
-    Rect sliderRect1 = (Offset(0,0))& Size(300.0, 3.0);
+    Rect sliderRect1 = (Offset(0, 0)) & Size(300.0, 3.0);
     canvas.drawRect(sliderRect1, fillPainter);
 
+    */
+    canvas.drawCircle(
+        Offset(size.width / 2, size.height - 50), 150, termPainter);
+
+    canvas.drawLine(Offset(size.width / 2,size.height-50),Offset(0,0) , Paint()
+      ..color = Colors.black
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 3.0);
+
   }
+  /*
+  _paintLine1(Canvas canvas1, Size size){
+    Rect sliderRect2 = (Offset(0, 0)) & Size(300.0, 3.0);
+    rotate(canvas1, size.width / 2, size.height - 50, 45);
+    canvas1.drawRect(sliderRect2, fillPainter);
+    rotate(canvas1, size.width / 2, size.height - 50, -45);
+  }
+  _paintLine2(Canvas canvas2, Size size){
+    Rect sliderRect2 = (Offset(0, 0)) & Size(300.0, 3.0);
+    rotate(canvas2, size.width/2, size.height-50, -45);
+    //rotate(canvas2, size.width / 2, size.height - 50, 180);
+    canvas2.drawRect(sliderRect2, fillPainter);
+    rotate(canvas2, size.width/2, size.height-50, 45);
+  }
+
+  void rotate(Canvas canvas1, double cx, double cy, double angle) {
+    canvas1.translate(cx, cy);
+    canvas1.rotate(angle);
+    canvas1.translate(-cx, -cy);
+  }
+
+   */
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
